d069deab19ecdd5ac52c989d7494744dfb9b62b2
logovol <pasha1985@inbox.ru>
2020-09-23 14:39:53 +0300

    homework-4 done

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    // 3 3
+    // 1 1 1
+    // 0 1 0
+    // 0 1 0
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 3;
+            int M = 3;
+
+            int[,] Map = { { 1, 1, 1, }, { 0, 1, 0 }, { 1, 1, 0 } };
+            int[,] A = new int[M, N];
+
+            bool zero = false;
+            for (int s = 0; s < N; s++)
+            {
+                if (zero)
+                {
+                    A[0, s] = 0;
+                    continue;
+                }
+
+                if (Map[0, s] == 0)
+                {
+                    A[0, s] = 0;
+                    zero = true;
+                }
+                else
+                    A[0, s] = 1;
+            }
+
+            zero = false;
+            for (int k = 0; k < M; k++)
+            {
+                if (zero)
+                {
+                    A[k, 0] = 0;
+                    continue;
+                }
+
+                if (Map[k, 0] == 0)
+                {
+                    A[k, 0] = 0;
+                    zero = true;
+                }
+                else
+                    A[k, 0] = 1;
+
+            }
+
+            int i, j;
+            for (i = 1; i < N; i++)
+            {
+                for (j = 1; j < M; j++)
+                {
+                    if (Map[i, j] == 1)
+                        A[i, j] = A[i, j - 1] + A[i - 1, j];
+                    else
+                        A[i, j] = 0;
+                }
+
+            }
+            Print(N, M, A);
+            Console.ReadLine();
+        }
+
+        static void Print(int n, int m, int[,] a)
+        {
+            int i, j;
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                    Console.Write($"    {a[i, j]}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ lab4/length/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1 = "geekbrains";
+            string word2 = "geekminds";
+            Console.WriteLine($"Слово №1 {word1}");
+            Console.WriteLine($"Слово №2 {word2}");            
+
+            int w1Length = word1.Length;
+            int w2Length = word2.Length;            
+
+            int[,] matrix = new int[w1Length + 1, w2Length + 1];
+
+            for (int i = 1; i <= w1Length; i++)
+            {
+                for (int j = 1; j <= w2Length; j++)
+                {                    
+                    if (word1[i - 1] == word2[j - 1])
+                    {
+                        matrix[i, j] = matrix[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = Math.Max(matrix[i - 1, j], matrix[i, j - 1]);
+                    }
+                }
+            }   
+
+            List<char> LCS = new List<char>();
+            int k = w1Length - 1, m = w2Length - 1;
+            while (k >= 0 && m >= 0)
+                if (word1[k] == word2[m])
+                { 
+                    LCS.Add(word1[k]);
+                    k--;
+                    m--;
+                }
+                else if (matrix[k - 1, m] > matrix[k, m - 1])
+                    k--;
+                else
+                    m--;
+            LCS.Reverse();
+
+            Console.Write($"Общая наибольшая подпоследовательность: ");
+            foreach (var item in LCS)
+            {
+                Console.Write(item);
+            }           
+                        
+            Console.WriteLine($"\nДлина: {matrix[w1Length, w2Length]}");
+            Console.ReadLine();
+        }
+    }
+}
