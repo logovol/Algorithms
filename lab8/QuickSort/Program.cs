aef0c5c911b13ac933b740290ae04fe128af2ba2
logovol <pasha1985@inbox.ru>
2020-10-07 16:02:33 +0300

    homework-8 done

---
+++ lab8/QuickSort/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static int[] arr = { 9, 3, 6, 7, 5, 3, 4 };
+        static void Main(string[] args)
+        {   
+            QuickS(0, arr.Length - 1);
+            Print();
+            Console.ReadKey();
+        }
+
+
+        static void QuickS(int left, int right)
+        {
+            int i = left; int j = right;
+            int x = arr[(left + right) / 2]; // средний элемент
+
+            do
+            {
+                while (arr[i] < x)
+                    // поиск элемента больше среднего
+                    i++;
+                while (arr[j] > x)
+                    // поиск элемента меньше среднего
+                    j--;
+                if (i <= j) // обмен элементов местами
+                {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                    i++;
+                    j--;
+                }
+            } while (i<=j);
+
+            if (left < j)
+                QuickS(left, j); // левый подмассив
+            if (i < right)
+                QuickS(i, right); // правый подмассив
+        }
+
+        static void Print()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+    }
+}
---
+++ lab8/SortBinaryInsert/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SortBinaryInsert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 7, 3, 9, 1 };
+
+            Sort(arr);
+            Print(arr);
+            Console.ReadKey();
+        }
+
+        private static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+
+        static void Sort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i <= n - 1; i++)
+            {
+                int tmp = arr[i];
+                int left = 0;
+                int right = i - 1;
+
+                while (left <= right)
+                {
+                    int m = (left + right) / 2;
+
+                    if (tmp < arr[m])
+                    {
+                        right = m - 1;
+                    }
+                    else
+                    {
+                        left = m + 1;
+                    }
+                }
+                for (int j = i - 1; j >= left; j--)
+                {
+                    arr[j + 1] = arr[j];                    
+                }
+                arr[left] = tmp;
+            }            
+        }
+    }
+}
